7e06d0d3fbce4ab773aed8881e2a5a707a377185
DeniArif <deniarifwibowo82@gmail.com>
2023-04-07 10:42:34 +0700

    comit

--- EASD_1_188_D/EASD_1_188_D.cpp
+++ EASD_1_188_D/EASD_1_188_D.cpp
@@ -7,5 +7,10 @@ int n;
 void selectionSort(int arr[88], int n) {
 	for (int j = 0; j < n - 1; j++) {
 		int min_index = j;
+		for (int i = j + 1; i < n; i++) {
+			if (arr[i] < arr[min_index]) {
+				min_index = i;
+			}
+		}
 	}
 }
